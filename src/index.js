3426a7d3eaab7adaf04db04291d5901dbb36ebc0
amrutha <yalamanchili.amrutha@gmail.com>
2020-02-05 12:54:51 -0500

    origin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import { choice, remove } from './helpers';
+import fruits from './fruits';
+var selected = choice(fruits);
+var removed = remove(fruits, selected);
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<p>I'd like one {selected} ,please</p>
+				<p>Here you go {selected}</p>
+				<p>Delicious ! May I Have Another one</p>
+				{removed};
+				<p>I m sorry ,we are all out.We have {removed.length} others fruits left</p>
+			</div>
+		);
+	}
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
