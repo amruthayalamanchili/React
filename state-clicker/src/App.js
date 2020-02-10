63ecec4504c46d08a6ee06cb3172da8a492fb1bc
amrutha <yalamanchili.amrutha@gmail.com>
2020-02-10 17:07:25 -0500

    new addons

---
+++ state-clicker/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Random from './Random';
+
+function App() {
+	return (
+		<div className="App">
+			<Random />
+		</div>
+	);
+}
+
+export default App;
